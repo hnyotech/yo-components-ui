4c59201da8d17b44e00581a024f1aac97c236943
张亚强 <jiushiyuana@163.com>
2019-08-27 15:21:34 +0800

    修改枚举

--- src/packages/YoEnum/src/main.vue
+++ src/packages/YoEnum/src/main.vue
@@ -1,62 +1,68 @@
 <template>
-<el-form-item :label="label + '：'" :prop="prop">
-    <el-select v-model="selectedValue" :placeholder="'请选择' + label" clearable :disabled="disabled" @change="handleChange">
-        <el-option v-for="item in options" :key="item.Value" :label="item.Description" :value="item.Value"></el-option>
+<el-form-item :label="label" :prop="prop">
+    <el-select v-model="selectedValue" clearable :disabled="disabled" :placeholder="'请选择' + label" @change="handleChange" @visible-change="visibleChange">
+        <el-option v-for="item in options" :key="item.Value" :label="item.Description" :value="item.Value">
+        </el-option>
     </el-select>
 </el-form-item>
 </template>
 
 <script type="text/javascript">
 export default {
-    name: "YoEnum",
-    props: {
-        value: [String, Number],
-        EnumName: {
-            type: String,
-            required: true
-        },
-        disabled: {
-            type: Boolean,
-            default: false,
-            required: false
-        },
-        label: {
-            type: String,
-            required: true
-        },
-        prop: {
-            type: String,
-            required: true
-        }
+  name: 'YoEnum',
+  props: {
+    selValue: {
+      type: [String, Number],
+      required: false,
+      default: ''
     },
-    data() {
-        return {
-            selectedValue: this.prop,
-            options: [{
-                Name:'张三',
-                Value:2,
-            }],
-        }
+    EnumName: {
+      type: String,
+      required: true
     },
-    watch: {
-        value(newVal, oldVal) {
-            this.selectedValue = newVal;
-        }
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false
     },
-    methods: {
-        bindEnumlist: function () {
-            var self = this;
-            this.$http.get("api/Enum/GetEnumItems?fullname=" + this.EnumName)
-                .then(function (res) {
-                    self.options = res;
-                });
-        },
-        handleChange(val) {
-            this.$emit("input", val);
-        }
+    label: {
+      type: String,
+      required: true
     },
-    mounted: function () {
-        this.bindEnumlist();
+    prop: {
+      type: String,
+      required: true
     }
+  },
+  data () {
+    return {
+      selectedValue: '',
+      options: []
+    }
+  },
+  created () {
+    this.selectedValue = this.selValue
+  },
+  watch: {
+    selValue: function (val) {
+      this.selectedValue = val
+    }
+  },
+  methods: {
+    bindEnumlist: function () {
+      var self = this
+      this.$http.get('api/Enum/GetEnumItems?fullname=' + this.EnumName)
+        .then(function (res) {
+          self.options = res
+        })
+    },
+    handleChange (val) {
+      this.$emit('selectchange', val)
+      console.log(val)
+    }
+  },
+  mounted: function () {
+    this.bindEnumlist()
+  }
 }
 </script>
