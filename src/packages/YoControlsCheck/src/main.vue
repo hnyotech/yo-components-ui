647254d825565fb6626f03ada223eb6720b8fbc6
张亚强 <jiushiyuana@163.com>
2019-09-17 14:54:07 +0800

    修改控件

--- src/packages/YoControlsCheck/src/main.vue
+++ src/packages/YoControlsCheck/src/main.vue
@@ -1,12 +1,31 @@
 <template>
   <el-col :span="proteyValue.ColPer">
     <i class="el-icon-error dynaattr" @click.stop="closez" v-if="isedit"></i>
-    <el-form-item :prop="itemsname + '.' + datakey + '.property.AttributeValue'">
-      <el-checkbox
+    <el-form-item
+      :label="proteyValue.Name"
+      :prop="itemsname + '.' + datakey + '.property.AttributeValue'"
+    >
+      <el-radio-group v-model="proteyValue.AttributeValue" v-if="proteyValue.AttributeType===5">
+        <el-radio
+          v-for="item in lstoptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >{{item.label}}</el-radio>
+      </el-radio-group>
+      <el-checkbox-group v-model="proteyValue.AttributeValue" v-else>
+        <el-checkbox
+          v-for="item in lstoptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-checkbox>
+      </el-checkbox-group>
+      <!-- <el-checkbox
         v-model="proteyValue.AttributeValue"
         true-label="true"
         false-label="false"
-      >{{proteyValue.Name}}</el-checkbox>
+      >{{proteyValue.Name}}</el-checkbox>-->
     </el-form-item>
   </el-col>
 </template>
@@ -49,6 +68,20 @@ export default {
         this.iscanedit = 1;
       }
       return this.iscanedit == 1;
+    },
+    lstoptions() {
+      let items = [];
+      let options = this.newproteyValue.ListOption;
+      if (options != undefined) {
+        let arr = options.split(",");
+
+        for (var i in arr) {
+          let str = arr[i];
+          items.push({ label: str, value: str });
+        }
+      }
+
+      return items;
     }
   },
 
