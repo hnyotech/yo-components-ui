0588d8f668cf67677a53d58c4a155f198324b401
李石涛 <lishitao@hnyotech.com.cn>
2020-04-22 14:41:22 +0800

    优化实现：1.快捷搜索，焦点在输入框中时候 回车直接触发查询；2.高级搜索 表单有焦点时候 回车直接触发查询；

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from "./store";
 /* ui plguin */
 import ElementUI from 'element-ui'
 import Auth from '@/auth'
@@ -27,6 +28,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
